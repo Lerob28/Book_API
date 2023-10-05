698673ce877d8e458965a6f42fe7fce24c6b0b4e
Lerob <njeunkweborel@gmail.com>
2023-10-05 13:52:49 +0100

    Implement basic error handling

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express'); 
-const mongoose = require('mongoose');
-const books = require('./books');
+const express =   require('express'); 
+const mongoose =  require('mongoose');
+const books =     require('./books');
 const bookModel = require('./book_model');
 const { authUser, authenticateJWT } = require('./auth');
 
@@ -9,8 +9,23 @@ const app = express();
 const database_uri = "mongodb+srv://njeunkweborel:pL3tOMBW83oJNDM4@cluster0.wy6yuha.mongodb.net/?retryWrites=true&w=majority";
 const host = "localhost";
 
+const normalizePort = (val) => {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+};
+
+const port = normalizePort(process.env.PORT || "4000");
+
 
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.json({message : 'API IS WORKING ...'});
@@ -24,38 +39,49 @@ app.get('/api/books', authenticateJWT, (req, res) => {
 });
 
 app.post('/api/book/add', authenticateJWT, (req, res) => {
-    let title = req.body.title;
-    let author = req.body.author;
-    let publishedDate = req.body.publishedDate;
+    if (!req.body.title && !req.body.author && !req.body.publishedDate) {
+        res.status(400).json({ message: "Content can not be empty!" });
+    }else {
+        let bookToSave = new bookModel({
+            title: req.body.title,
+            author: req.body.author,
+            publishedDate: req.body.publishedDate
+        }); 
 
-    let bookToSave = new bookModel({
-        title: title,
-        author: author,
-        publishedDate: publishedDate
-    }); 
-
-    bookToSave
-    .save()
-    .then(bookSaved => {
-        res.status(201).json({
-            message: 'book successfully store to database !',
-            book: bookSaved
+        bookToSave
+        .save()
+        .then(bookSaved => {
+            res.status(201).json({
+                message: 'book successfully save !',
+                book: bookSaved
+            });
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while saving book"
+            });
         });
-    })
-    .catch(err => console.log('err', err));
+    }
+    
 });
 
 
 app.put('/api/book/edit/:id', authenticateJWT, (req, res) => {
-    let id = req.params.id;
+    if(!req.body.title && !req.body.author && !req.body.publishedDate) {
+        res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }else {
+        let id = req.params.id;
 
-    bookModel.findByIdAndUpdate(id, req.body, { useFindAndModify: false, new: true })
-    .then((data) => 
-        console.log(data)
-    ).catch(err => console.log(err));
-
-    res.status(200).json("the book have been successfully update");
-
+        bookModel.findByIdAndUpdate(id, req.body, { useFindAndModify: false, new: true })
+        .then((book) => {
+            res.status(200).json({message: "the book have been successfully update", book});
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while updating book"
+            });
+        });
+    }
 });
 
 
@@ -63,24 +89,34 @@ app.delete('/api/book/delete/:id', authenticateJWT, (req, res) => {
     let id = req.params.id;
 
     bookModel.findByIdAndDelete(id)
-    .then((data) => 
-        console.log(data)
-    ).catch(err => console.log(err));
+    .then((book) =>{
+        if (!book) {
+            res.status(404).send({
+              message: `book not found.`
+            });
+          } else {
+            res.send({
+              message: "the book have been successfully delete!"
+            });
+          }
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        });
+    });
 
-    res.status(200).json("the book have been delete");
 });
 
 
 app.post('/api/auth', authUser);
-
+  
 
 mongoose.connect(database_uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log(`app is succefuly connected to codevigor Database...`);
 
-    const server = app.listen(5000, function(){
-        var port = server.address().port
-        console.log("REST API demo app listening at http://%s:%s", host, port)
+    app.listen(port, function(){
+        console.log("REST API demo app listening on port %s", port)
     });
 
   })
